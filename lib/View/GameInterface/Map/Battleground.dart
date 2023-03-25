76e66a8b3c5559c551f9008aba830c9ef7cc3b08
HazmiHazim <hazimnetto01@gmail.com>
2023-03-25 22:29:08 +0800

    New Update

--- lib/View/GameInterface/Map/Battleground.dart
+++ lib/View/GameInterface/Map/Battleground.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Battleground extends StatelessWidget {
+class Battleground extends StatefulWidget {
   const Battleground({Key? key}) : super(key: key);
+
+  @override
+  State<Battleground> createState() => _BattlegroundState();
+}
+
+class _BattlegroundState extends State<Battleground> {
 
   @override
   Widget build(BuildContext context) {
@@ -10,10 +16,19 @@ class Battleground extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset(
-            'assets/images/Grass-Battle-Ground.png',
-            width: MediaQuery.of(context).size.width - 30,
-            fit: BoxFit.cover,
+          Stack(
+            children: [
+              Image.asset(
+                'assets/images/Grass-Battle-Ground.png',
+                width: MediaQuery.of(context).size.width - 30,
+                fit: BoxFit.cover,
+              ),
+              AnimatedContainer(
+                duration: Duration(seconds: 3),
+                curve: Curves.easeInOut,
+                child: Image.asset('assets/images/Boy.png', width: 120, height: 120),
+              ),
+            ],
           ),
           Image.asset(
             'assets/images/Battle-Box.png',
@@ -25,3 +40,4 @@ class Battleground extends StatelessWidget {
     );
   }
 }
+
